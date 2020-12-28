8c0b7ae36ddd4ee535f2461db4117ce51c4eb25f
micael <micael@DESKTOP-0LJ7MBB>
2020-12-28 16:52:14 -0300

    passdoor

--- init.lua
+++ init.lua
@@ -1,31 +1,77 @@
 --Users
 local Users = {
     Prefeito = 1234,
-    Engenheiro = 3654
+    Engenheiro = 1937
 }
 --Sleep
-
 function Wait(seconds)
     local start = os.time()
     repeat until os.time() > start + seconds
 end
-
 --Clear Function 
-
 function Clear()
     os.execute("cls")
 end
+--Funções para abrir as portas
 
---Init2
-function Funclist()
-    print("1 - Abrir Porta Central \n2 - Abrir porta Reactor")
-    if io.read() == "1" then
+--Porta Central
+function CentralDoor()
         Clear()
-        print("Sua porta foi aberta!")
-        Wait(3)
+    print("Sua porta foi aberta!")
+        Wait(1.5)
+        Clear()
+end
+
+--Porta Reator
+function NukeDoor()
+        Clear()
+    print("Sua porta da Nuke foi aberta!") 
+        Wait(1.5)
+        Clear()
+end
+
+--Abrir Todas as Portas
+function AllDoors()
+    Clear()
+        print("Todas as portas foram abertas!") 
+        Wait(1.5)
+        Clear()   
+end
+
+--Prefeito Init2
+function Funclist0()
+    print("1 - Abrir Porta Central \n2 - Abrir porta Reactor")
+    local inside = io.read()
+    if inside == "1" then
+         CentralDoor()
+    elseif inside == "2" then
+         AllDoors()
     end
     
 end
+
+--Engenheiro Init2
+
+function Funclist1()
+    print("Digite o numero da funcao! \nVoce tem acesso a essas funcoes: \n1 - Abrir Porta Central \n2 - Abrir porta Reactor \n3 - Abrir Todas as Portas")
+        _G.wrong = true
+        local inside = io.read()
+        --Adicionar Funções 
+    if inside == "1" then
+        CentralDoor() -- Função de abrir Central Door
+    elseif inside == "2" then
+        NukeDoor()
+    elseif inside == "3" then
+        AllDoors()
+    else 
+        Clear()
+        print("A funcao " .. inside .. " nao existe! Tente novamente!")
+        Wait(1.5)
+        Clear()
+        Funclist1()
+    end
+end
+
 --Mecanismo de login 
 
 local wrong = false 
@@ -41,15 +87,14 @@ while true do
     if input == Users.Prefeito then 
         Clear()
         print("Bem vindo Prefeito")
-        Funclist()
-            Wait(3)
         Clear()
-    elseif input == Users.Prefeito2 then
+        Funclist0()
+    elseif input == Users.Engenheiro then
         Clear()
         print("Bem vindo Engenheiro Chefe")
-        Funclist() -- Funclist Função
-        --Wait(3) -- Função Sleep
-        --Clear()
+        Clear()
+        Funclist1() 
+        
     else
         wrong = true  
     end
